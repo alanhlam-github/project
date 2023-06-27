3f5c185e12986e9b36a85f442b1d16518d6680ec
Alan Lam <alanhlam@hotmail.com>
2023-06-27 18:23:03 -0400

    update

--- Pet Store script.R
+++ Pet Store script.R
@@ -1,41 +1,41 @@
-inu=import('https://github.com/alanhlam-github/dataset/blob/main/pet%20store.xlsx')
+pet=import('https://github.com/alanhlam-github/dataset/blob/main/pet%20store.xlsx')
 
-table(is.na(inu))
-table(duplicated(inu))
+table(is.na(pet))
+table(duplicated(pet))
 
 #Retention rate
-sum(duplicated(inu$cust_id))/sum(!duplicated(inu$cust_id))*100
+sum(duplicated(pet$cust_id))/sum(!duplicated(pet$cust_id))*100
 
 #Customer age
-inu |> 
+pet |> 
   ggplot(aes(x=cust_age))+
   theme_bw()+
   geom_bar(color='darkred')+
   labs(y='Count',x='Customer Age',title='Total Count of Customers by Age')+
   theme(plot.title = element_text(hjust=.5))
 
-inu |> 
+pet |> 
   tabyl(cust_age) |> 
   adorn_pct_formatting(digits = 1,affix_sign = T)|> 
   arrange(desc(n)) |> 
   datatable(colnames=c('Age'=2,'Count'=3,'Percentage'=4),options=list(searching=F))
 
 #Create new DF to sort cust_state in desc order
-inu2=data.frame(States=fct_infreq(inu$cust_state))
+pet2=data.frame(States=fct_infreq(pet$cust_state))
 
-#Added scale_y_discrete(limits = rev(levels(inu2$States))) to reverse ascending order
-inu2 |> 
-  ggplot(aes(y=States,fill=inu$cust_state %in% c('New York','Pennsylvania','New Jersey')))+
+#Added scale_y_discrete(limits = rev(levels(pet2$States))) to reverse ascending order
+pet2 |>
+  ggplot(aes(y=States,fill=pet$cust_state %in% c('New York','Pennsylvania','New Jersey')))+
   theme_bw()+
   geom_bar()+
-  scale_y_discrete(limits = rev(levels(inu2$States))) +
-  labs(y='States',x='Number of Transactions',title=' States by Transactions')+
+  scale_y_discrete(limits = rev(levels(pet2$States))) +
+  labs(y='US State',x='Transactions',title=' States by Transactions')+
   theme(plot.title=element_text(hjust=.5))+
   guides(fill=F)+
   scale_fill_manual(values = c('red','darkgreen'))
 
 #Rename variables in order
-inu |> 
+pet |> 
   rename('State'=cust_state) |> 
   tabyl('State') |> 
   adorn_pct_formatting(digits = 1,affix_sign = T) |> 
@@ -44,10 +44,10 @@ inu |>
   rename('Customers'=n) |> 
   datatable()
 
-#Create inu3 to order prod category
-inu3=data.frame(prod_category=fct_infreq(inu$prod_category),prod_animal_type=inu$prod_animal_type)
+#Create pet3 to order prod category
+pet3=data.frame(prod_category=fct_infreq(pet$prod_category),prod_animal_type=pet$prod_animal_type)
 
-inu3 |> 
+pet3 |> 
   ggplot(aes(x=prod_category,fill=prod_animal_type))+
   geom_bar()+
   theme_bw()+
@@ -55,7 +55,7 @@ inu3 |>
   theme(plot.title = element_text(hjust=.5))+
   scale_fill_manual(values = c('red','lightblue'))
 
-inu |> 
+pet |> 
   rename(`Product Category`=prod_category) |> 
   tabyl(`Product Category`,prod_animal_type) |>
   adorn_totals() |> 
@@ -66,15 +66,15 @@ inu |>
   datatable(rownames = T,colnames = c('Cat'=3,'Dog'=4),options=list(searching=F))
 
 #create DF to round to two decimals
-inu_rounded=data.frame(prod_title=inu$prod_title,prod_animal_type=inu$prod_animal_type,total_sales=round(inu$total_sales,2))
+pet_rounded=data.frame(prod_title=pet$prod_title,prod_animal_type=pet$prod_animal_type,total_sales=round(pet$total_sales,2))
 
-inu4=inu_rounded |> 
+pet4=pet_rounded |> 
   group_by('Product Title'=prod_title,'Animal Type'=prod_animal_type) |>
   summarise(Sales=sum(total_sales)) |> 
   arrange(-Sales)
 
 #Use reorder() to sort descending
-inu4 |> 
+pet4 |> 
   ggplot(aes(x=Sales, y=reorder(`Product Title`,Sales),fill=Sales > 119000))+
   theme_bw()+
   geom_bar(stat='identity')+
@@ -83,7 +83,7 @@ inu4 |>
   guides(fill=F)+
   theme(plot.title=(element_text(hjust=.5)))
 
-datatable(inu4,colnames=c('Total Sales'='Sales')) |> 
+datatable(pet4,colnames=c('Total Sales'='Sales')) |> 
   formatCurrency(columns=c('Total Sales'))
 
 sessionInfo()
